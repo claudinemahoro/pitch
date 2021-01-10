b6b0ec1cdac659fb8acdd447460d3f42c02ef374
claudinemahoro <claudinemah3@gmail.com>
2021-01-10 21:20:33 +0200

    testing the models'methods

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,14 +1,13 @@
+
 import unittest
 from app.models import User
 
 class UserModelTest(unittest.TestCase):
-
     def setUp(self):
-        self.new_user = User(password = 'banana')
-
+        self.new_user=User(password='banana')
+    
     def test_password_setter(self):
-
-        self.assertTrue(self.new_user.password_hash is not None)
+        self.assertTrue(self.new_user.pass_secure is not None)
 
     def test_no_access_password(self):
         with self.assertRaises(AttributeError):
@@ -16,3 +15,4 @@ class UserModelTest(unittest.TestCase):
 
     def test_password_verification(self):
         self.assertTrue(self.new_user.verify_password('banana'))
+  
