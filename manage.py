456e69f7e5306fbebea9e214bc9cd8941edba034
claudinemahoro <claudinemah3@gmail.com>
2021-01-10 21:56:39 +0200

    deployment

--- manage.py
+++ manage.py
@@ -3,11 +3,8 @@ from flask_script import Manager,Server
 from app.models import User,Pitch,Comment,Upvote,Downvote,PhotoProfile
 from  flask_migrate import Migrate, MigrateCommand
 # Creating app instance
-# app = create_app('test')
-app = create_app('development')
-# app = create_app('production')
 
-
+app = create_app('production')
 
 manager = Manager(app)
 migrate = Migrate(app,db)
